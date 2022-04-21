486eeb900cb601ab766cad7a260c3deb62f7ccf8
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-20 20:13:40 -0400

    created test for left button visible

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,21 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, fireEvent } from "@testing-library/react";
+import AppFunctional from "./AppFunctional";
+
+test("sanity", () => {
+  expect(false).toBe(false);
+});
+
+beforeEach(() => {
+  render(<AppFunctional />);
+});
+
+describe("Key Pad", () => {
+  test("left is present", () => {
+    const leftBttn = screen.queryByText("LEFT");
+    expect(leftBttn).toBeVisible();
+    expect(leftBttn).toBeInTheDocument();
+  });
+});
